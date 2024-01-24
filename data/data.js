700392dadb1c4abc113e1a6eebc0443cdd80ee5a
unknown <dannailhams@gmail.com>
2024-01-25 01:43:38 +0700

    File Upload nextJs

---
+++ data/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first: 'John',
+            last: 'Doe',
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '15 Minutes ago',
+    },
+    {
+        id: 2,
+        name: {
+            first: 'JMalaka',
+            last: 'Kiw',
+        },
+        total: 2795.95,
+        status: 'Processing',
+        method: 'PayPal',
+        date: '15 Minutes ago',
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Lawson',
+            last: 'Sin',
+        },
+        total: 2795.95,
+        status: 'Completed',
+        method: 'PayPal',
+        date: '15 Minutes ago',
+    },
+    {
+        id: 4,
+        name: {
+            first: 'Sarah',
+            last: 'Mith',
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '15 Minutes ago',
+    },
+]
